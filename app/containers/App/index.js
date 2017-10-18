d890e2dadd20643005864e0c273473064b016d0f
Szymon W <szymon.wojciechowski@sequencesh.pl>
2017-10-18 19:06:13 +0200

    init commit

---
+++ app/containers/App/index.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * App.react.js
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ */
+
+import React from 'react';
+import Helmet from 'react-helmet';
+
+// Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
+import 'sanitize.css/sanitize.css';
+
+import Img from 'components/Img';
+import Footer from 'components/Footer';
+import Banner from './banner-metal.jpg';
+import A from 'components/A';
+
+import styles from './styles.css';
+
+
+import elo from './no';
+function App(props) {
+  return (
+    <div className={styles.wrapper}>
+      <div>hello worldsss</div>
+      <Helmet
+        titleTemplate="%s - React.js Boilerplate"
+        defaultTitle="React.js Boilerplate"
+        meta={[
+          { name: 'description', content: 'A React.js Boilerplate application' },
+        ]}
+      />
+      <A className={styles.logoWrapper} href="https://twitter.com/mxstbr">
+        <Img className={styles.logo} src={Banner} alt="react-boilerplate - Logo" />
+      </A>
+      {React.Children.toArray(props.children)}
+      <Footer />
+    </div>
+  );
+}
+
+App.propTypes = {
+  children: React.PropTypes.node,
+};
+
+export default App;
